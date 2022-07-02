30d8197800af7672abae2edabba988c3b658dd1a
gregory-bopp <gregory-bopp@github.com>
2022-07-02 16:51:39 -0400

    Change InvGamma prior parameter names

--- R/inv_gamma_prior.R
+++ R/inv_gamma_prior.R
@@ -1,37 +1,49 @@
-InvGamma <- R6::R6Class("InvGamma",
-                inherit = Prior,
-                public =
-                  list(name = "InvGamma",
-                       ig_shape = 1,
-                       ig_scale = 1
-                  )
+InvGamma <- R6::R6Class(
+  "InvGamma",
+  inherit = Prior,
+  public =
+    list(
+      name = "InvGamma",
+      shape = 1,
+      scale = 1,
+      rate = 1
+    )
 )
 
 InvGamma$set('public',
-          'initialize',
-  function(ig_shape,
-           ig_scale){
-    if(!missing(ig_shape))
-      self$ig_shape = ig_shape
-    if(!missing(ig_scale))
-    self$ig_scale = ig_scale
-    invisible(self)
-  }
-)
+             'initialize',
+             function(shape,
+                      rate,
+                      scale = 1/rate){
+               if (!missing(rate) && !missing(scale)) {
+                stop("specify 'rate' or 'scale' but not both")
+               }
+               if (!missing(shape))
+                 self$shape = shape
+               if (!missing(scale)){
+                 self$scale = scale
+                 self$rate = 1/scale
+               }
+               if(!missing(rate)){
+                 self$scale = 1/rate
+                 self$rate = 1/scale
+               }
+               invisible(self)
+             })
 
 InvGamma$set('public',
-               'dprior',
- function(x){
-   if (x > 0) {
-     ll <- dgamma(1 / x,
-                  shape = self$ig_shape,
-                  rate = self$ig_scale,
-                  log = T) -
-       2 * log(x) # Jacobian
-   }
-   else{
-     ll <- -Inf
-   }
- }
-)
-
+             'dprior',
+             function(x) {
+               if (all(x > 0)) {
+                 ll <- sum(dgamma(
+                   1 / x,
+                   shape = self$shape,
+                   rate = self$scale,
+                   log = T
+                 ) -
+                   2 * log(x)) # Jacobian
+               }
+               else{
+                 ll <- -Inf
+               }
+             })
